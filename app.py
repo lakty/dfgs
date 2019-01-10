e611619d077716f780b1cb170b5ba097c99e3f07
Lakty <la.v@i.ua>
2019-01-10 18:34:56 +0200

    Добавил вторую страницу

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from config import Config
 from constant import events, fine
 import datetime
-
+import json
 import forms
 
 app = Flask(__name__)
@@ -13,14 +13,28 @@ app.config.from_object(Config)
 def crib_form():
     data_form = forms.CribForm(request.form)
     data_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    if request.method == 'POST' and data_form.validate():
-        return redirect(url_for("crib"))
+    if request.method == 'POST' and data_form.is_submitted():
+        input_data = {
+            "first_name": data_form.first_name.data,
+            "last_name": data_form.last_name.data,
+            "middle_name": data_form.middle_name.data,
+            "date_birthday": data_form.date_birthday.data,
+            "document": data_form.document.data,
+            "reg_place": data_form.reg_place.data,
+            "date_time_event": data_form.date_time_event.data,
+            "place_event": data_form.place_event.data,
+            "vehicle": data_form.vehicle.data,
+            "incident": data_form.incident.data,
+            "witness": data_form.witness.data,
+            "fine": data_form.fine.data,
+            "police_bio": data_form.police_bio.data,
+            "police_rang": data_form.police_rang.data,
+            "police_position": data_form.police_position.data
+        }
+        print(input_data['police_position'])
+        return render_template("crib/index.html", input_data=input_data)
+
     return render_template("crib_form/index.html", data_form=data_form, data_time=data_time)
-
-
-@app.route('/crib')
-def crib():
-    return render_template("crib/index.html")
 
 
 @app.route('/fine/<incident>')
--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from constant import events
 from wtforms import StringField, DateTimeField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from wtforms.fields.html5 import DateField
 
 
-class CribForm(Form):
+class CribForm(FlaskForm):
     first_name = StringField("Ім\'я", validators=[DataRequired()])
     last_name = StringField("Прізвище", validators=[DataRequired()])
     middle_name = StringField("По батькові", validators=[DataRequired()])
-    date_birthday = DateField("Дата народження", validators=[DataRequired()], format='%Y-%m-%d')
+    date_birthday = DateField("Дата народження")
     document = StringField("Документ", validators=[DataRequired()])
     reg_place = StringField("Зареєстрований", validators=[DataRequired()])
     date_time_event = DateTimeField("Дата(час) розгляду", validators=[DataRequired()])
@@ -19,4 +19,7 @@ class CribForm(Form):
                            choices=[("", "Порушення"), *[(events[event], event) for event in events.keys()]])
     witness = StringField("ПІБ свідка", validators=[DataRequired()])
     fine = StringField("Cтягнення", validators=[DataRequired()])
+    police_bio = StringField("ПІБ", validators=[DataRequired()])
+    police_rang = StringField("Спеціальне звання", validators=[DataRequired()])
+    police_position = StringField("Посада", validators=[DataRequired()])
     submit = SubmitField("Переглянути")
