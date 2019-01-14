7ffd87116e763ce14ec03f3864d01f1a57959b4e
Lakty <la.v@i.ua>
2019-01-14 20:42:29 +0200

    Убрал " д/н 4435"

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from config import Config
 from constant import events
 import datetime
 import forms
+import pytz
+time_zone = pytz.timezone('Europe/Kiev')
 
 app: Flask = Flask(__name__)
 app.config.from_object(Config)
@@ -17,7 +19,7 @@ def check():
 @app.route('/', methods=['GET', 'POST'])
 def crib_form():
     data_form = forms.CribForm(request.form)
-    data_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    data_time = time_zone.localize(datetime.datetime.now()).strftime("%d-%m-%Y %H:%M:%S")
     if request.method == 'POST' and data_form.is_submitted():
         input_data = {
             "first_name": data_form.first_name.data,
